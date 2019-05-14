e5236eeed63178cf750ec17488b893123a353c4a
AMMAR <ammarna1985@gmail.com>
2019-05-13 21:11:24 -0400

    Version_04_Ammar

---
+++ Web_Pages/config.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$dbname = 'universitydb';
+$user = 'root';
+$pass = '';
+$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+try {
+# MySQL with PDO_MYSQL
+  $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  echo 'Connected';
+}
+catch(PDOException $e) {
+  echo $e->getMessage();
+}
+?>
--- Web_Pages/index.php
+++ Web_Pages/index.php
@@ -1,3 +1,7 @@
+<?php
+  include("config.php");
+ ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,151 +11,133 @@
       <style>
        .carousel-inner > .item > img,
        .carousel-inner > .item > a > img {
-         width: 70%;
+         width: 100%;
          margin: auto;
        }
       </style>
   </head>
 
   <body>
-    <div class="container-fluid">
-      <nav class="navbar navbar-inverse">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Final Project</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li ><a href="#">Home</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Students <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Teachers <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Courses<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
- </nav>
-<br>
- <div id="myCarousel" class="carousel slide " data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-
-      <div class="item active">
-        <img src="./Images/student.jpg" alt="Students" width="460" height="345">
-        <div class="carousel-caption">
-          <h3>Students</h3>
-          <p>description</p>
+     <div class="container-fluid">
+          <nav class="navbar navbar-inverse">
+              <div class="navbar-header">
+                <a class="navbar-brand" href="#">Final Project</a>
+              </div>
+              <ul class="nav navbar-nav">
+                  <li ><a href="#">Home</a></li>
+                  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Students <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Page 1-1</a></li>
+                      <li><a href="#">Page 1-2</a></li>
+                      <li><a href="#">Page 1-3</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Teachers <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Page 1-1</a></li>
+                      <li><a href="#">Page 1-2</a></li>
+                      <li><a href="#">Page 1-3</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Courses<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Page 1-1</a></li>
+                      <li><a href="#">Page 1-2</a></li>
+                      <li><a href="#">Page 1-3</a></li>
+                    </ul>
+                  </li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+              <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+              </ul>
+          </nav>
         </div>
-      </div>
+          <!--Slider-->
+          <div id="myCarousel" class="carousel slide " data-ride="carousel" style="height:400px;">
+          <ol class="carousel-indicators">
+               <?php
 
-      <div class="item">
-        <img src="./Images/teacher.jpg" alt="Teachers" width="460" height="345">
-        <div class="carousel-caption">
-          <h3>Teachers</h3>
-          <p>description.</p>
-        </div>
-      </div>
+                       $STH = $DBH->query('SELECT * FROM slider');
+                       # setting the fetch mode
+                       $STH->setFetchMode(PDO::FETCH_ASSOC);
+                       $counter=0;
 
-      <div class="item">
-        <img src="./Images/course.jpg" alt="Courses" width="460" height="345">
-        <div class="carousel-caption">
-          <h3>courses</h3>
-          <p>Description.</p>
-        </div>
-      </div>
-    </div>
+                       while($row = $STH->fetch()) {
+                  ?>
+                          <li data-target="#myCarousel" data-slide-to="<?php echo $counter; ?>" <?php if($counter==0) {echo 'class="active"';} ?> ></li>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+                  <?php
+                          $counter=$counter + 1;
+                      }
+                  ?>
+                  </ol>
+                  <div class="carousel-inner" role="listbox">
+                       <?php
+                           $STH = $DBH->query('SELECT * FROM slider');
+                           # setting the fetch mode
+                           $STH->setFetchMode(PDO::FETCH_ASSOC);
+                           $counter=0;
+
+                           while($row = $STH->fetch()) {
+                          ?>
+                              <div <?php if($counter==0) {echo 'class="item active"';} else {echo 'class="item"';}?>>
+                                <a href="<?php echo $row['Link_Address']; ?>">
+                                  <img src="<?php echo $row['Image']; ?>"  style="height:400px;">
+                                </a>
+                                <div class="carousel-caption">
+                                  <h3><?php echo $row['Header_Text']; ?></h3>
+                                  <p><?php echo $row['Description_Text']; ?></p>
+                                </div>
+                              </div>
+                      <?php
+                          $counter=$counter + 1;
+                          }
+                      ?>
+
+                  </div>
+                  <!-- Wrapper for slides -->
+
+
+
+
+
+                  <!-- Left and right controls -->
+                  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                  </a>
+
 </div>
 
   <br><br><br>
 <div class="container">
  <div class="row">
-    <div class="col-sm-6 col-md-4 col-lg-3">
-		<div class="thumbnail">
-			<a href="https://www.vaniercollege.qc.ca/">
-				<img src="./Images/student1.jpg"/>
-			</a>
-				<div class="caption">
-					<h1>Students</h1>
-					<p>Description </p>
-					<button class="btn btn-success">More</button>
-				</div>
-		</div>
-     </div>
+   <?php
 
-    <div class="col-sm-6 col-md-4 col-lg-3">
-		<div class="thumbnail">
-			<a href="https://www.vaniercollege.qc.ca/">
-			<img src="./Images/teacher1.jpg"/>
-			</a>
-				<div class="caption">
-					<h1>Teachers</h1>
-					<p>Description</p>
-					<button class="btn btn-success">More</button>
-				</div>
-        </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4 col-lg-3">
-		<div class="thumbnail">
-			<a href="https://www.vaniercollege.qc.ca/">
-				<img src="./Images/course1.jpg"/>
-			</a>
-				<div class="caption">
-					<h1>Courses</h1>
-					<p>Description </p>
-					<button class="btn btn-success">More</button>
-				</div>
-        </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4 col-lg-3">
-		<div class="thumbnail">
-			<a href="https://www.vaniercollege.qc.ca/">
-				<img src="./Images/news.jpg"/>
-			</a>
-				<div class="caption">
-					<h1>Latest news</h1>
-					<p>Description </p>
-					<button class="btn btn-success">More</button>
-				</div>
-        </div>
-    </div>
+              $STH = $DBH->query('SELECT * FROM thumbnail');
+              # setting the fetch mode
+              $STH->setFetchMode(PDO::FETCH_ASSOC);
+              while($row = $STH->fetch()) {
+       ?>
+                 <div class="col-sm-6 col-md-4 col-lg-3" >
+                   <div class="thumbnail">
+                     <a href="<?php echo $row['Link_Address']; ?>">
+                       <img src="<?php echo $row['Image']; ?>" style="width:150; height:175px;"/>
+                     </a>
+                     <div class="caption">
+             					<h1><?php echo $row['Header_Text']; ?></h1>
+             					<p><?php echo $row['Description_Text']; ?></p>
+             				</div>
+                   </div>
+                 </div>
+           <?php
+               }
+         ?>
  </div>
 </div>
 
