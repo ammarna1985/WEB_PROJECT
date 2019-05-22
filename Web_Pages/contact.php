ba03a7478a49e39630cf03922e5984d6ec83bfb2
Mohammad <signal_1983@yahoo.com>
2019-05-22 13:36:23 -0400

    Version_10_Mohammad

--- Web_Pages/contact.php
+++ Web_Pages/contact.php
@@ -9,15 +9,55 @@
           include("head.php");
          ?>
         <style>
-          p {color:blue;}
-        </style>
-    </head>
+              * {
+                box-sizing: border-box;
+              }
 
-    <body>
+              /* Create two equal columns that floats next to each other */
+              .column {
+                float: left;
+                padding: 10px;
+                height: 520px; /* Should be removed. Only for demonstration */
+              }
 
-      <p>Address: Velenjak st. Tehran, Iran</p><br>
-      <p>Telephone: +98 21 222244</p></br>
-      <p>Email: info@sbu.edu.ir</p>
+              /* Clear floats after the columns */
+              .row:after {
+                content: "";
+                display: table;
+                clear: both;
+              }
+
+            .right  {
+              width: 30%;
+            }
+
+            .left  {
+              width: 70%;
+            }
+
+      </style>
+  </head>
+
+<body>
+
+<div class="container-fluid">
+      <div class="row">
+            <div class="column left" style="background-color:;">
+                <iframe style="width:100% ;height: 100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.035460063806!2d51.39249771524393!3d35.79906278016694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e062ec6ff7d4f%3A0xcc54d2b761710b81!2sShahid+Beheshti+University!5e0!3m2!1sen!2sca!4v1558542271041!5m2!1sen!2sca"  frameborder="0" style="border:0" allowfullscreen>
+                </iframe>
+            </div>
+            <div class="column right" style="margin-top:25px">
+                <h1 style="text-align:center;">Contact Information</h1>
+                <hr>
+                <p><b>Address:</b> Velenjak st. Daneshjou Boulevard, District 1</p>
+                <p>Tehran, Tehran</p>
+                <p>19839 69411,Iran</p><br>
+                <p><b>Phone:</b>+98 21 222244</p>
+                <p><b>Email:</b> info@sbu.edu.ir</p>
+                <p><b>Fax:</b>+98 21 22433455</p>
+            </div>
+      </div>
+</div>
 
       <?php
         include("footer.php");
