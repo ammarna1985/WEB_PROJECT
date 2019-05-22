66cd3633e5aad8a124114466ab0f087e71bf820b
Mohammad <signal_1983@yahoo.com>
2019-05-22 18:15:51 -0400

    Version_11_Mohammad

--- Web_Pages/contact.php
+++ Web_Pages/contact.php
@@ -27,13 +27,7 @@
                 clear: both;
               }
 
-            .right  {
-              width: 30%;
-            }
 
-            .left  {
-              width: 70%;
-            }
 
       </style>
   </head>
@@ -42,12 +36,13 @@
 
 <div class="container-fluid">
       <div class="row">
-            <div class="column left" style="background-color:;">
+         <div class="col-sm-12">
+            <div class="column" style="width: 70%;">
                 <iframe style="width:100% ;height: 100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.035460063806!2d51.39249771524393!3d35.79906278016694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e062ec6ff7d4f%3A0xcc54d2b761710b81!2sShahid+Beheshti+University!5e0!3m2!1sen!2sca!4v1558542271041!5m2!1sen!2sca"  frameborder="0" style="border:0" allowfullscreen>
                 </iframe>
             </div>
-            <div class="column right" style="margin-top:25px">
-                <h2 style="text-align:left;">Contact Information</h2>
+            <div class="column">
+                <h1 style="text-align:center;">Contact Information</h1>
                 <hr>
                 <p><b>Address:</b> Velenjak st. Daneshjou Boulevard, District 1</p>
                 <p>Tehran, Tehran</p>
@@ -56,7 +51,8 @@
                 <p><b>Email:</b> info@sbu.edu.ir</p>
                 <p><b>Fax:</b>+98 21 22433455</p>
             </div>
-      </div>
+        </div>
+    </div>
 </div>
 
       <?php
